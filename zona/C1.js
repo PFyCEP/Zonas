e24879874ec5f59526283b6368fece8f0695bea7
PFyCEP <50671894+PFyCEP@users.noreply.github.com>
2019-05-30 11:01:11 -0300

    Add files via upload

--- zona/C1.js
+++ zona/C1.js
@@ -3,17 +3,8 @@ var Comercio = {
           "type": "FeatureCollection",
           "features": [
 
-       { "geometry": { "type": "Point", "coordinates": [ -60.62971374, -32.95412855] },
-                "type": "Feature", "properties": { "popupContent": "Alem Leandro N. 1271 " }, "id": 750},
-
-       { "geometry": { "type": "Point", "coordinates": [ -60.63645148, -32.94941604] },
-                "type": "Feature", "properties": { "popupContent": "San Luis 932 " }, "id": 1744},
-
        { "geometry": { "type": "Point", "coordinates": [ -60.64585913, -32.93707441] },
                 "type": "Feature", "properties": { "popupContent": "Italia 143 " }, "id": 1845},
-
-       { "geometry": { "type": "Point", "coordinates": [ -60.63387453, -32.95243643] },
-                "type": "Feature", "properties": { "popupContent": "Buenos Aires 1193 " }, "id": 4536},
 
        { "geometry": { "type": "Point", "coordinates": [ -60.6505519, -32.94935242] },
                 "type": "Feature", "properties": { "popupContent": "Dorrego 1182 " }, "id": 5038},
@@ -29,14 +20,6 @@ var Comercio = {
 
        { "geometry": { "type": "Point", "coordinates": [ -60.63506754, -32.95101825] },
                 "type": "Feature", "properties": { "popupContent": "San Juan 801 " }, "id": 7042},
-       { "geometry": { "type": "Point", "coordinates": [ -60.63769282, -32.94919436] },
-                "type": "Feature", "properties": { "popupContent": "San Luis 1034 " }, "id": 7090},
-
-       { "geometry": { "type": "Point", "coordinates": [ -60.64112212, -32.94780162] },
-                "type": "Feature", "properties": { "popupContent": "Ricardone 1253 " }, "id": 7096},
-
-       { "geometry": { "type": "Point", "coordinates": [ -60.64125076, -32.94777826] },
-                "type": "Feature", "properties": { "popupContent": "Ricardone 1269 " }, "id": 7097},
 
        { "geometry": { "type": "Point", "coordinates": [ -60.65107112, -32.95139352] },
                 "type": "Feature", "properties": { "popupContent": "Dorrego 1362 " }, "id": 7398},
@@ -46,26 +29,14 @@ var Comercio = {
 
        { "geometry": { "type": "Point", "coordinates": [ -60.65032391, -32.95487008] },
                 "type": "Feature", "properties": { "popupContent": "Italia 1639 " }, "id": 7457},
-
        { "geometry": { "type": "Point", "coordinates": [ -60.64756984, -32.95457496] },
                 "type": "Feature", "properties": { "popupContent": "Presidente Roca 1568 " }, "id": 7637},
 
        { "geometry": { "type": "Point", "coordinates": [ -60.6378218, -32.9410818] },
                 "type": "Feature", "properties": { "popupContent": "Pasaje Simeoni 1160 " }, "id": 7906},
 
-       { "geometry": { "type": "Point", "coordinates": [ -60.64024244, -32.9485602] },
-                "type": "Feature", "properties": { "popupContent": "Mitre 968 " }, "id": 7937},
-
-       { "geometry": { "type": "Point", "coordinates": [ -60.64109001, -32.95276845] },
-                "type": "Feature", "properties": { "popupContent": "Mitre 1319 " }, "id": 7956},
-
-       { "geometry": { "type": "Point", "coordinates": [ -60.63716013, -32.94911739] },
-                "type": "Feature", "properties": { "popupContent": "San Martín 983 " }, "id": 8035},
        { "geometry": { "type": "Point", "coordinates": [ -60.63024075, -32.95027875] },
                 "type": "Feature", "properties": { "popupContent": "Primero De Mayo 975 " }, "id": 8271},
-
-       { "geometry": { "type": "Point", "coordinates": [ -60.63485544, -32.95722639] },
-                "type": "Feature", "properties": { "popupContent": "Montevideo 673 " }, "id": 11389},
 
        { "geometry": { "type": "Point", "coordinates": [ -60.64444986, -32.94394291] },
                 "type": "Feature", "properties": { "popupContent": "Santa Fe 1569 " }, "id": 11722},
@@ -73,9 +44,6 @@ var Comercio = {
        { "geometry": { "type": "Point", "coordinates": [ -60.64499056, -32.95582136] },
                 "type": "Feature", "properties": { "popupContent": "Corrientes 1636 " }, "id": 11808},
 
-       { "geometry": { "type": "Point", "coordinates": [ -60.64081406, -32.95166914] },
-                "type": "Feature", "properties": { "popupContent": "Mitre 1229 " }, "id": 11815},
-
        { "geometry": { "type": "Point", "coordinates": [ -60.64970784, -32.95163116] },
-                "type": "Feature", "properties": { "popupContent": "Italia 1352 " }, "id": 12040} ] };
+                "type": "Feature", "properties": { "popupContent": "Italia 1352 " }, "id": 12040},
 
