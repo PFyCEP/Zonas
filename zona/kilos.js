adc31f84c3babf31e6273c438f08b1643bc99a72
PFyCEP <50671894+PFyCEP@users.noreply.github.com>
2019-07-25 11:28:41 -0300

    Add files via upload

---
+++ zona/kilos.js
@@ -0,0 +1,203 @@
+﻿var nivel1 = {
+          "type": "FeatureCollection",
+          "features": [
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66180344, -32.93496244] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 120" }, "id": 6853},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.663347, -32.93900375] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 130" }, "id": 9145},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65296446, -32.9340071] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 133" }, "id": 7414},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65544828, -32.9409167] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 134" }, "id": 14375},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66119156, -32.93529044] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 144" }, "id": 9135},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6574672, -32.9356411] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 146" }, "id": 6840},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6608982, -32.9341308] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 153" }, "id": 13017},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6592731, -32.93412455] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 192" }, "id": 193},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6578221, -32.9332106] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 223" }, "id": 9101},
+       { "geometry": { "type": "Point", "coordinates": [ -60.66170202, -32.93249121] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 225" }, "id": 689},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65213989, -32.93304356] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 232" }, "id": 7260},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65686392, -32.93582558] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 254" }, "id": 9089},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.660945, -32.93995484] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 318" }, "id": 9128},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6574808, -32.9318562] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 321" }, "id": 9094},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6524368, -32.9351807] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 328" }, "id": 9049},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65426956, -32.93679951] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 346" }, "id": 3873},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65988608, -32.9414162] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 351" }, "id": 9111},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6536421, -32.93888075] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 352" }, "id": 10569},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65851744, -32.93666302] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 366" }, "id": 6531},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65802115, -32.93439694] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 385" }, "id": 12587},
+       { "geometry": { "type": "Point", "coordinates": [ -60.66121162, -32.94095255] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 457" }, "id": 2291},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65608668, -32.93759648] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 470" }, "id": 153},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65288268, -32.9340218] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 479" }, "id": 7246},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6534389, -32.940136] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 483" }, "id": 6232},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65471746, -32.9434234] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 497" }, "id": 4740},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66176276, -32.93486976] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 498" }, "id": 6852},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65323884, -32.93756516] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 499" }, "id": 9059} ] };
+
+var nivel2 = {
+          "type": "FeatureCollection",
+          "features": [
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.654642, -32.93748278] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 503" }, "id": 9070},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65169442, -32.93312426] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 561" }, "id": 622},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65866612, -32.93656688] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 562" }, "id": 9108},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65198866, -32.93307098] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 626" }, "id": 2953},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65198061, -32.9354774] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 633" }, "id": 11844},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6577576, -32.9337497] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 716" }, "id": 2519},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.653833, -32.9407124] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 868" }, "id": 9063},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6615031, -32.9421664] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 888" }, "id": 9130},
+       { "geometry": { "type": "Point", "coordinates": [ -60.65444369, -32.94312953] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 921" }, "id": 9066},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65274622, -32.9356149] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 923" }, "id": 9054},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65303947, -32.93287988] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 933" }, "id": 7282} ] };
+
+var nivel3 = {
+          "type": "FeatureCollection",
+          "features": [
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6525079, -32.9328766] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1069" }, "id": 12118},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65582068, -32.93349464] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1111" }, "id": 14306},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66127259, -32.94204526] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1261" }, "id": 2059},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65198532, -32.93186428] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1274" }, "id": 7888},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6640865, -32.9411395] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1350" }, "id": 9147},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65371664, -32.93506656] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1417" }, "id": 7232},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65514647, -32.93464764] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1439" }, "id": 8248} ] };
+
+var nivel4 = {
+          "type": "FeatureCollection",
+          "features": [
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66125867, -32.93381767] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1543" }, "id": 7240},
+       { "geometry": { "type": "Point", "coordinates": [ -60.65514647, -32.93464764] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1546" }, "id": 8248},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65203085, -32.93357255] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1558" }, "id": 9045},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65764322, -32.93570951] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1587" }, "id": 6842},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65490532, -32.94338972] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1639" }, "id": 4741},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65293916, -32.93530564] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1653" }, "id": 991},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6612948, -32.93261235] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1676" }, "id": 7253},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66003106, -32.9327391] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 1865" }, "id": 6014} ] };
+
+var nivel5 = {
+          "type": "FeatureCollection",
+          "features": [
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66143732, -32.93492832] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 2015" }, "id": 6850},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66035189, -32.93275747] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 2102" }, "id": 9133},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.662116, -32.9311] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 2235" }, "id": 7292},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66217735, -32.9361058] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 2383" }, "id": 6535},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66127259, -32.94204526] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 2622" }, "id": 2059},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65819018, -32.9428007] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 3247" }, "id": 4747},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65707286, -32.94232068] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 4613" }, "id": 1134},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.66113464, -32.93585786] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 5483" }, "id": 2313},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65851744, -32.93666302] },
+                "type": "Feature", "properties": { "popupContent": "Kilos: 5561" }, "id": 6531} ] };
+
